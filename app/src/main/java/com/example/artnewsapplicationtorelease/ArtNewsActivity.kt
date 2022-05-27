f9d35253959d97fa50fc3d35a69ec44c4e578378
Maxim-Lysik <emerzozz@gmail.com>
2022-05-27 03:05:27 +0300

    Get request done

--- app/src/main/java/com/example/artnewsapplicationtorelease/ArtNewsActivity.kt
+++ app/src/main/java/com/example/artnewsapplicationtorelease/ArtNewsActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -32,7 +33,10 @@ class ArtNewsActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_artnews)
+      //  val navController = findNavController(R.id.nav_host_fragment_activity_artnews)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_artnews) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -44,3 +48,18 @@ class ArtNewsActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 }
+
+/*class ArtNewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val newsRepository = NewsRepository(NewsDataBase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+        val navController = findNavController(R.id.nav_host_fragment_activity_artnews)
+    }
+}*/
