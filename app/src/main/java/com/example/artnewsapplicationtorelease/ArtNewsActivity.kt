b5bbdb40992a3155fb33e285d2b98955cc7b3a8f
Maxim-Lysik <emerzozz@gmail.com>
2022-07-18 22:43:35 +0300

    commit mond (Conditions_part_done1)

--- app/src/main/java/com/example/artnewsapplicationtorelease/ArtNewsActivity.kt
+++ app/src/main/java/com/example/artnewsapplicationtorelease/ArtNewsActivity.kt
@@ -2,6 +2,7 @@ package com.example.artnewsapplicationtorelease
 
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +20,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fragment_stats.*
 import kotlinx.android.synthetic.main.fragment_stats.view.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ArtNewsActivity : AppCompatActivity() {
@@ -59,21 +61,51 @@ class ArtNewsActivity : AppCompatActivity() {
 
         var calendar_point1 = Calendar.getInstance()
         var day = calendar_point1[Calendar.DAY_OF_WEEK].toInt()
+        var day_time = calendar_point1.time
+        var month = calendar_point1[Calendar.MONTH]
+        var year = calendar_point1[Calendar.YEAR]
+        var day_day = calendar_point1[Calendar.DAY_OF_MONTH]
+        val corrected_date = calendar_point1.set(Calendar.MONTH,Calendar.YEAR, Calendar.DAY_OF_MONTH )
+        val myFormat = "dd-MM-yyyy"
+        var fullDateFormatter = SimpleDateFormat(myFormat)
+        var final_date = fullDateFormatter.parse("$day_day-$month-$year")
+        var string_for_date: String = ("$day_day-$month-$year").toString()
+
+
+
+
+        Log.d(TAG, "CALENDAR TODAY'S ISS ${day_time}");
 
        // sharedPreference.edit().putInt("Point1", day).commit()
       //  sharedPreference.edit().putInt("Point2", day).commit()
 
 
-        if(sharedPreference.getInt("Point1", 0) == 0){
-        sharedPreference.edit().putInt("Point1", day).commit()
+        if(sharedPreference.getString("Point1", "") == ""){
+        sharedPreference.edit().putString("Point1", string_for_date).commit()
        }
 
-        else if(sharedPreference.getInt("Point1", 0) != 0 && sharedPreference.getInt("Point2", 0) ==0 ){
+        else if(sharedPreference.getString("Point1", "") != "" && sharedPreference.getString("Point2", "") == "" ){
 
 
-            sharedPreference.edit().putInt("Point2", day).commit()
+            sharedPreference.edit().putString("Point2", string_for_date).commit()
 
         }
+
+      else if( (sharedPreference.getString("Point1", "") != "" && sharedPreference.getString("Point2", "") != "")
+                  && fullDateFormatter.parse(sharedPreference.getString("Point2", ""))!! >= fullDateFormatter.parse(sharedPreference.getString("Point1", ""))   ){
+
+            sharedPreference.edit().putString("Point1", sharedPreference.getString("Point2", "")).commit()  // Последнюю дату ставим на место первой
+            sharedPreference.edit().putString("Point2", string_for_date).commit()
+            Log.d(TAG, "USPEKH");
+
+
+       }
+
+
+
+
+       // Log.d(TAG, "FORMATTER 1 IS ${fullDateFormatter.parse(sharedPreference.getString("Point2", ""))}");
+      //  Log.d(TAG, "FORMATTER 2 IS ${fullDateFormatter.parse(sharedPreference.getString("Point1", ""))}");
 
 
 
@@ -162,8 +194,8 @@ class ArtNewsActivity : AppCompatActivity() {
         val sharedPreference =
             getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
 
-        var time_point1 = sharedPreference.getInt("Point1", 0)
-        var time_point2 = sharedPreference.getInt("Point2", 0)
+        var time_point1 = sharedPreference.getString("Point1", "")
+        var time_point2 = sharedPreference.getString("Point2", "")
 
        /* if(sharedPreference.getInt("Point1", 0)==0){
             val calendar_point1 = Calendar.getInstance()
