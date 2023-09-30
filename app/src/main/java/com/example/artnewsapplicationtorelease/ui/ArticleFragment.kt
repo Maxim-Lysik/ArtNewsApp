9330dc2a039d822d3f942a8246dd3bf016856ce2
Maxone <emerzozz@gmail.com>
2023-09-30 06:44:45 +0300

    commit mond (Saved5.210)

--- app/src/main/java/com/example/artnewsapplicationtorelease/ui/ArticleFragment.kt
+++ app/src/main/java/com/example/artnewsapplicationtorelease/ui/ArticleFragment.kt
@@ -70,7 +70,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         //  bundle.putString("project", projectName)
         fragment.arguments = bundle
 
-// test56
+// test57
         /*val hui = args.shit
         Log.d(ContentValues.TAG, "SUKAAA: ${hui}")*/
 
