058df4060b725db18afb3fce54ee39e42e92a6f9
Maxone <emerzozz@gmail.com>
2024-04-01 21:43:06 +0300

    commit mond (Saved5.315)

--- app/src/main/java/com/example/artnewsapplicationtorelease/ui/ArticleFragment.kt
+++ app/src/main/java/com/example/artnewsapplicationtorelease/ui/ArticleFragment.kt
@@ -70,7 +70,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         //  bundle.putString("project", projectName)
         fragment.arguments = bundle
 
-// test162
+// test163
 
 
         /*val fragment_stats = StatsFragment()
